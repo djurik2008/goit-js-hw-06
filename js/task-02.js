01716613428f80930acf5a4cba9e7de20a70207c
Oleh Tymoshenko <djurik2008@gmail.com>
2023-10-15 17:44:01 +0300

    finish task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = ingredients.map(ingredient => {
+  const bodyUlEl = document.createElement('li')
+  bodyUlEl.textContent = ingredient
+  bodyUlEl.classList.add('item')
+  return bodyUlEl
+})
+
+const ingredientsListContainer = document.querySelector('#ingredients')
+ingredientsListContainer.append(...ingredientsList)
+
+
+
+
