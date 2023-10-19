04a40c0087b13c1a556518d54fc12f69aa12e72f
Oleh Tymoshenko <djurik2008@gmail.com>
2023-10-19 17:53:32 +0300

    removed unnecessary code

--- js/task-07.js
+++ js/task-07.js
@@ -2,8 +2,6 @@ const inputFontSizeControl = document.querySelector('#font-size-control');
 
 const textEl = document.querySelector('#text');
 
-console.dir(textEl);
-
 inputFontSizeControl.addEventListener('change', onInputRangeCheck);
 
 function onInputRangeCheck(event) {
