341ba1cfa7b48d432c54d67e899c4ee175d2eedc
Oleh Tymoshenko <djurik2008@gmail.com>
2023-10-21 11:57:15 +0300

    completed task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widgetButton = document.querySelector('.change-color');
+
+widgetButton.addEventListener('click', onWidgetBtnChangeColor);
+
+function onWidgetBtnChangeColor() {
+  const randomColor = getRandomHexColor();
+  const colorNameSpanEl = document.querySelector('.color');
+  const bodyEl = document.querySelector('body');
+
+  bodyEl.style.backgroundColor = randomColor;
+  colorNameSpanEl.textContent = randomColor;
+}
